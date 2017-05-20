e186f8567b9ec90c8bb7bd6e0b7ecd3d6635fb1d
marcomussi <marco.mussi@mail.polimi.it>
2017-05-20 12:45:18 +0200

    aggiunto parser spazio mercato

---
+++ src/main/java/it/polimi/ingsw/ps22/parser/MarketZoneSaxParser.java
@@ -0,0 +1,136 @@
+package it.polimi.ingsw.ps22.parser;
+
+/*
+
+XML Structure:
+<spaces>   
+    <space>
+        <Coin>1</Coin>
+        <Stone>1</Stone>
+        <Wood>1</Wood>
+        <Servant>1</Servant>
+        <MilitaryPoint>1</MilitaryPoint>
+        <FaithPoint>1</FaithPoint>
+        <VictoryPoint>1</VictoryPoint>
+        <CouncilPrivilege>1</CouncilPrivilege>
+    </space>    
+</spaces>
+
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import it.polimi.ingsw.ps22.resource.*;
+import it.polimi.ingsw.ps22.resource.ResourceAbstract;
+
+public class MarketZoneSaxParser {
+
+	public static void MarketParser(String pathname, ArrayList<HashMap<String, ResourceAbstract>> parsedData) 
+	{
+		try {
+		   SAXParserFactory factory = SAXParserFactory.newInstance();
+		   SAXParser saxParser = factory.newSAXParser();
+		   DefaultHandler handler = new DefaultHandler() {
+			   HashMap<String, ResourceAbstract> toAdd = new HashMap<String, ResourceAbstract>();
+			   int lastCoin = 0; 
+			   int lastStone = 0; 
+			   int lastWood = 0; 
+			   int lastServant = 0; 
+			   int lastMilitaryPoint = 0; 
+			   int lastFaithPoint = 0; 
+			   int lastVictoryPoint = 0;
+			   int lastCouncilPrivilege = 0;
+			   boolean boolCoin = false;
+			   boolean boolStone = false;
+			   boolean boolWood = false; 
+			   boolean boolServant = false;
+			   boolean boolMilitaryPoint = false;
+			   boolean boolFaithPoint = false;
+			   boolean boolVictoryPoint = false;
+			   boolean boolCouncilPrivilege = false;
+			   
+			   
+			   public void startElement(String uri, String localName,String qName, Attributes attributes) throws SAXException {
+				   //System.out.println("Start Element :" + qName);
+				   if (qName.equalsIgnoreCase("Coin")) {boolCoin = true;}
+				   if (qName.equalsIgnoreCase("Stone")) {boolStone = true;}
+				   if (qName.equalsIgnoreCase("Wood")) {boolWood = true;}
+				   if (qName.equalsIgnoreCase("Servant")) {boolServant = true;}
+				   if (qName.equalsIgnoreCase("MilitaryPoint")) {boolMilitaryPoint = true;}
+				   if (qName.equalsIgnoreCase("FaithPoint")) {boolFaithPoint = true;}
+				   if (qName.equalsIgnoreCase("VictoryPoint")) {boolVictoryPoint = true;} 
+				   if (qName.equalsIgnoreCase("CouncilPrivilege")) {boolCouncilPrivilege = true;}
+				   if (qName.equalsIgnoreCase("space")) {
+					   toAdd = new HashMap<String, ResourceAbstract>();
+				   }
+				  
+			   }
+
+			   public void endElement(String uri, String localName, String qName) throws SAXException {
+				   //System.out.println("End Element :" + qName);
+				   if (qName.equalsIgnoreCase("space"))
+				   		{
+					   	parsedData.add(toAdd);
+				   		//lastEra=new ArrayList<Integer>();
+				   		//lastName=new StringBuffer();
+				   		}
+			   		}
+
+			   public void characters(char ch[], int start, int length) throws SAXException {
+				   	if (boolCoin) 
+			   			{lastCoin= Integer.parseInt(new String(ch, start, length));
+			   			toAdd.put("Coin", new Coin(lastCoin));
+			   			boolCoin = false;}
+				   
+				   	if (boolStone) 
+			   			{lastStone= Integer.parseInt(new String(ch, start, length));
+			   			toAdd.put("Stone", new Stone(lastStone));
+			   			boolStone = false;}
+				   
+				   	if (boolWood) 
+			   			{lastWood= Integer.parseInt(new String(ch, start, length));
+			   			toAdd.put("Wood", new Coin(lastWood));
+			   			boolWood = false;}
+				   
+				   	if (boolServant) 
+			   			{lastServant= Integer.parseInt(new String(ch, start, length));
+			   			toAdd.put("Servant", new Servant(lastServant));
+			   			boolServant = false;}
+				   
+				   	if (boolMilitaryPoint) 
+				   		{lastMilitaryPoint= Integer.parseInt(new String(ch, start, length));
+				   		toAdd.put("MilitaryPoint", new MilitaryPoint(lastMilitaryPoint));
+				   		boolMilitaryPoint = false;}
+				   
+			   		if (boolFaithPoint) 
+			   			{lastFaithPoint= Integer.parseInt(new String(ch, start, length));
+			   			toAdd.put("FaithPoint", new FaithPoint(lastFaithPoint));
+			   			boolFaithPoint = false;}
+			   		
+			   		if (boolVictoryPoint) 
+		   				{lastVictoryPoint= Integer.parseInt(new String(ch, start, length));
+		   				toAdd.put("VictoryPoint", new VictoryPoint(lastVictoryPoint));
+		   				boolVictoryPoint = false;}
+			   		
+			   		if (boolCouncilPrivilege) 
+		   				{lastCouncilPrivilege= Integer.parseInt(new String(ch, start, length));
+		   				toAdd.put("CouncilPrivilege", new CouncilPrivilege(lastCouncilPrivilege));
+		   				boolCouncilPrivilege = false;}
+				   
+			   		}
+		   		};
+
+		   		
+		   	saxParser.parse(pathname, handler);
+
+	} catch (Exception e) 
+	   	{e.printStackTrace();}
+
+	}
+	
+}
