76bc2aa3298579fb5b64e66e9defbba084e315a8
marcomussi <marco.mussi@mail.polimi.it>
2017-05-23 23:42:06 +0200

    ottimizzazione codice del parser delle carte edificio

--- src/main/java/it/polimi/ingsw/ps22/parser/BuildingCardSaxParser.java
+++ src/main/java/it/polimi/ingsw/ps22/parser/BuildingCardSaxParser.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.ps22.parser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -10,22 +9,19 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import it.polimi.ingsw.ps22.action.CardAction;
-import it.polimi.ingsw.ps22.action.HarvestAction;
-import it.polimi.ingsw.ps22.action.ProductionAction;
 import it.polimi.ingsw.ps22.card.CardBuilding;
-import it.polimi.ingsw.ps22.effect.EndVictoryEffect;
 import it.polimi.ingsw.ps22.effect.ExchangeResource;
-import it.polimi.ingsw.ps22.effect.ExtraAction;
 import it.polimi.ingsw.ps22.effect.GainResource;
 import it.polimi.ingsw.ps22.effect.MultiplyEffect;
 import it.polimi.ingsw.ps22.resource.Coin;
 import it.polimi.ingsw.ps22.resource.CouncilPrivilege;
 import it.polimi.ingsw.ps22.resource.FaithPoint;
 import it.polimi.ingsw.ps22.resource.MilitaryPoint;
-import it.polimi.ingsw.ps22.resource.ResourceAbstract;
+import it.polimi.ingsw.ps22.resource.Point;
+import it.polimi.ingsw.ps22.resource.Resource;
 import it.polimi.ingsw.ps22.resource.Servant;
 import it.polimi.ingsw.ps22.resource.Stone;
+import it.polimi.ingsw.ps22.resource.VictoryPoint;
 import it.polimi.ingsw.ps22.resource.Wood;
 
 /*
@@ -34,15 +30,12 @@ import it.polimi.ingsw.ps22.resource.Wood;
 		<name>BASE</name>													
 		<era>0</era>
 		<prodreq>0</prodreq>
-		
 		<cost>
             <coin>0</coin>	
             <wood>0</wood>		
             <stone>0</stone>
             <servant>0</servant>
-            <military>0</military>
         </cost>
-							
 		<gainEffect>														
     		<coin>0</coin>													
     		<wood>0</wood>													
@@ -53,7 +46,6 @@ import it.polimi.ingsw.ps22.resource.Wood;
     		<councilpoint>0</councilpoint>
     		<victorypoint>0</victorypoint>									
 		</gainEffect>
-
 		<gainprodeffect>														
     		<coin>0</coin>													
     		<wood>0</wood>													
@@ -64,7 +56,6 @@ import it.polimi.ingsw.ps22.resource.Wood;
     		<councilpoint>0</councilpoint>
     		<victorypoint>0</victorypoint>	
 		</gainprodeffect>
-
 		<exchangeprodeffect>														
     		<coin>0</coin>													
     		<wood>0</wood>													
@@ -75,7 +66,6 @@ import it.polimi.ingsw.ps22.resource.Wood;
     		<councilpoint>0</councilpoint>
     		<victorypoint>0</victorypoint>	
 		</exchangeprodeffect>
-
 		<cardmoltprodeffect>														
     		<coin>0</coin>													
     		<wood>0</wood>													
@@ -87,13 +77,12 @@ import it.polimi.ingsw.ps22.resource.Wood;
     		<victorypoint>0</victorypoint>
     		<type>CardBuilding</type>
 		</cardmoltprodeffect>
-
     </card>	
 */
 
 public class BuildingCardSaxParser {
 
-	public static void VentureCardParser(String pathname, ArrayList<CardBuilding> parsedData) {
+	public static void BuildingCardParser(String pathname, ArrayList<CardBuilding> parsedData) {
 		try {
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
@@ -102,40 +91,19 @@ public class BuildingCardSaxParser {
 				GainResource gainImmEffect = new GainResource();
 				GainResource gainProdEffect = new GainResource();
 				ExchangeResource exchangeProdEffect = new ExchangeResource();
-				MultiplyEffect multProdEffect;
-				CardAction cardAct;
+				MultiplyEffect multProdEffect = new MultiplyEffect();
+				String lastQName;
 				int lastInt = 0;
-				int lastImmActionValue = 0;
-				boolean boolName = false;
-				boolean boolEra = false;
 				boolean boolCost = false;
 				boolean boolGainImm = false;
 				boolean boolGainProd = false;
 				boolean boolExchangeProd = false;
 				boolean boolCardMoltProd = false;
-				boolean boolCoin = false;
-				boolean boolStone = false;
-				boolean boolWood = false;
-				boolean boolServant = false;
-				boolean boolMilitaryPoint = false;
-				boolean boolCouncilPoint = false;
-				boolean boolFaithPoint = false;
-				boolean boolVictoryPoint = false;
-				boolean boolType = false;
 
-				// ridefinizione del metodo startElement all'interno del
-				// DefaultHandler
 				public void startElement(String uri, String localName, String qName, Attributes attributes)
 						throws SAXException {
 
-					// Boolean basilari della carta
-					if (qName.equalsIgnoreCase("name")) {
-						boolName = true;
-					}
-
-					if (qName.equalsIgnoreCase("era")) {
-						boolEra = true;
-					}
+					lastQName = qName.toLowerCase();
 
 					// Boolean per capire in quale ambito stiamo parlando della
 					// risorsa
@@ -159,143 +127,165 @@ public class BuildingCardSaxParser {
 						boolCardMoltProd = true;
 					}
 
-					// Boolean Risorse
-					if (qName.equalsIgnoreCase("coin")) {
-						boolCoin = true;
-					}
-
-					if (qName.equalsIgnoreCase("stone")) {
-						boolStone = true;
-					}
-
-					if (qName.equalsIgnoreCase("wood")) {
-						boolWood = true;
-					}
-
-					if (qName.equalsIgnoreCase("servant")) {
-						boolServant = true;
-					}
-
-					if (qName.equalsIgnoreCase("military")) {
-						boolMilitaryPoint = true;
-					}
-
-					if (qName.equalsIgnoreCase("councilpoint")) {
-						boolCouncilPoint = true;
-					}
-
-					if (qName.equalsIgnoreCase("faithpoint")) {
-						boolFaithPoint = true;
-					}
-
-					if (qName.equalsIgnoreCase("victorypoint")) {
-						boolVictoryPoint = true;
-					}
-
-					// Boolean tipo di carta da modificare
-					if (qName.equalsIgnoreCase("type")) {
-						boolType = true;
-					}
-
 				}
 
-				// ridefinizione del metodo endElement all'interno del
-				// DefaultHandler
 				public void endElement(String uri, String localName, String qName) throws SAXException {
 
+					if (qName.equalsIgnoreCase("cost")) {
+						boolCost = false;
+					}
+
+					if (qName.equalsIgnoreCase("gaineffect")) {
+						card.addImmediateEffect(gainImmEffect);
+						gainImmEffect = new GainResource();
+						boolGainImm = false;
+					}
+
+					if (qName.equalsIgnoreCase("gainprodeffect")) {
+						card.addActionEffect(gainProdEffect);
+						gainProdEffect = new GainResource();
+						boolGainProd = false;
+					}
+
+					if (qName.equalsIgnoreCase("exchangeprodeffect")) {
+						card.addActionEffect(exchangeProdEffect);
+						exchangeProdEffect = new ExchangeResource();
+						boolExchangeProd = false;
+					}
+
+					if (qName.equalsIgnoreCase("cardmoltprodeffect")) {
+						card.addActionEffect(multProdEffect);
+						multProdEffect = new MultiplyEffect();
+						boolCardMoltProd = false;
+					}
+
+					if (qName.equalsIgnoreCase("card")) {
+						parsedData.add(card);
+						card = new CardBuilding();
+					}
+
 				}
 
-				// ridefinizione del metodo characters all'interno del
-				// DefaultHandler
 				public void characters(char ch[], int start, int length) throws SAXException {
+					String str = new String(ch, start, length);
+					if (lastQName.equalsIgnoreCase("name"))
+						card.setName(str);
+					else {
+						if (lastQName.equalsIgnoreCase("type"))
+							multProdEffect.setMultiplier(str);
+						else {
+							lastInt = Integer.parseInt(str);
 
-					// aggiunto nome alla carta
-					if (boolName) {
-						card.setName(new String(ch, start, length));
-						boolName = false;
+							if (lastQName.equalsIgnoreCase("era"))
+								card.setEra(lastInt);
+
+							if (lastQName.equalsIgnoreCase("prodreq"))
+								card.setActionValue(lastInt);
+
+							if (lastQName.equalsIgnoreCase("coin"))
+								processElementResource(new Coin(lastInt));
+
+							if (lastQName.equalsIgnoreCase("stone"))
+								processElementResource(new Stone(lastInt));
+
+							if (lastQName.equalsIgnoreCase("wood"))
+								processElementResource(new Wood(lastInt));
+
+							if (lastQName.equalsIgnoreCase("servant"))
+								processElementResource(new Servant(lastInt));
+
+							if (lastQName.equalsIgnoreCase("militarypoint"))
+								processElementPoint(new MilitaryPoint(lastInt));
+
+							if (lastQName.equalsIgnoreCase("faithpoint"))
+								processElementPoint(new FaithPoint(lastInt));
+
+							if (lastQName.equalsIgnoreCase("victorypoint"))
+								processElementPoint(new VictoryPoint(lastInt));
+
+							if (lastQName.equalsIgnoreCase("coucilpoint"))
+								processElementCouncilPrivilege(lastInt);
+
+						}
 					}
 
-					// aggiunta era alla carta
-					if (boolEra) {
-						lastInt = Integer.parseInt(new String(ch, start, length));
-						card.setEra(lastInt);
-						boolEra = false;
-					}
-					
-					// gestione monete
-					if (boolCoin) {
-						lastInt = Integer.parseInt(new String(ch, start, length));
-						if (boolCost) {
-							card.addCost("Coin", new Coin(lastInt));
-						} 
-						if (boolGainImm) {
-							gainImmEffect.addGain("Coin", new Coin(lastInt));
-						}
-						if (boolExchangeProd) {
-							if (lastInt<0) {
-								exchangeProdEffect.addCost("Coin", new Coin(lastInt));
-								}
-							else {
-								exchangeProdEffect.addGain("Coin", new Coin(lastInt));
-								}
-						}
-						if (boolCardMoltProd) {
-							multProdEffect.setMultiplicandType("Coin");
-							multProdEffect.setMultiplicand(new Coin(lastInt));
-						}
-						boolCoin = false;
-					}
-					
-					// gestione pietre
-					if (boolStone) {
-						lastInt = Integer.parseInt(new String(ch, start, length));
-						if (boolCost) {
-							card.addCost("Stone", new Stone(lastInt));
-						} 
-						if (boolGainImm) {
-							gainImmEffect.addGain("Stone", new Stone(lastInt));
-						}
-						if (boolExchangeProd) {
-							if (lastInt<0) {
-								exchangeProdEffect.addCost("Stone", new Stone(lastInt));
-								}
-							else {
-								exchangeProdEffect.addGain("Stone", new Stone(lastInt));
-								}
-						}
-						if (boolCardMoltProd) {
-							multProdEffect.setMultiplicandType("Stone");
-							multProdEffect.setMultiplicand(new Stone(lastInt));
-						}
-						boolStone = false;
-					}
-					
-					// gestione legni
-					if (boolWood) {
-						lastInt = Integer.parseInt(new String(ch, start, length));
-						if (boolCost) {
-							card.addCost("Wood", new Wood(lastInt));
-						} 
-						if (boolGainImm) {
-							gainImmEffect.addGain("Wood", new Wood(lastInt));
-						}
-						if (boolExchangeProd) {
-							if (lastInt<0) {
-								exchangeProdEffect.addCost("Wood", new Wood(lastInt));
-								}
-							else {
-								exchangeProdEffect.addGain("Wood", new Wood(lastInt));
-								}
-						}
-						if (boolCardMoltProd) {
-							multProdEffect.setMultiplicandType("Wood");
-							multProdEffect.setMultiplicand(new Wood(lastInt));
-						}
-						boolWood = false;
-					}
-					
-					
 				}
+
+				private void processElementResource(Resource res) {
+					if (boolCost) {
+						card.addCost(res.getName(), res);
+					} else {
+						if (boolGainImm) {
+							gainImmEffect.addGain(res.getName(), res);
+						} else {
+							if (boolGainProd) {
+								gainProdEffect.addGain(res.getName(), res);
+							} else {
+								if (boolExchangeProd) {
+									if (lastInt < 0) {
+										exchangeProdEffect.addCost(res.getName(), res);
+									} else {
+										exchangeProdEffect.addGain(res.getName(), res);
+									}
+								} else {
+									if (boolCardMoltProd) {
+										multProdEffect.setMultiplicandType(res.getName());
+										multProdEffect.setMultiplicand(res);
+									}
+								}
+							}
+						}
+					}
+				}
+
+				private void processElementPoint(Point point) {
+
+					if (boolGainImm) {
+						gainImmEffect.addGain(point.getName(), point);
+					} else {
+						if (boolGainProd) {
+							gainProdEffect.addGain(point.getName(), point);
+						} else {
+							if (boolExchangeProd) {
+								if (lastInt < 0) {
+									exchangeProdEffect.addCost(point.getName(), point);
+								} else {
+									exchangeProdEffect.addGain(point.getName(), point);
+								}
+							} else {
+								if (boolCardMoltProd) {
+									multProdEffect.setMultiplicandType(point.getName());
+									multProdEffect.setMultiplicand(point);
+								}
+							}
+						}
+					}
+				}
+
+				private void processElementCouncilPrivilege(int value) {
+
+					if (boolGainImm) {
+						gainImmEffect.addGain("CouncilPoint", new CouncilPrivilege(value));
+					} else {
+						if (boolGainProd) {
+							gainProdEffect.addGain("CouncilPoint", new CouncilPrivilege(value));
+						} else {
+							if (boolExchangeProd) {
+								if (lastInt < 0) {
+									exchangeProdEffect.addCost("CouncilPoint", new CouncilPrivilege(value));
+								} else {
+									exchangeProdEffect.addGain("CouncilPoint", new CouncilPrivilege(value));
+								}
+							} else {
+								if (boolCardMoltProd) {
+									multProdEffect.setMultiplicandType("CouncilPoint");
+									multProdEffect.setMultiplicand(new CouncilPrivilege(value));
+								}
+							}
+						}
+					}
+				}
+
 			};
 
 			saxParser.parse(pathname, handler);
