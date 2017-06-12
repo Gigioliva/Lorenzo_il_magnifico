bb28f66b6883d6bead771434064a9d185c1cdc46
Gigioliva <gianluigi.oliva@mail.polimi.it>
2017-06-12 17:23:50 +0200

    utilizzo java.awt.Color

--- src/main/java/it/polimi/ingsw/ps22/server/model/ColorPlayer.java
+++ src/main/java/it/polimi/ingsw/ps22/server/model/ColorPlayer.java
@@ -1,21 +1,30 @@
 package it.polimi.ingsw.ps22.server.model;
 
+
 public enum ColorPlayer {
-	RED(1), BLUE(2), GREEN(3), YELLOW(4);
-	
+	RED(1, java.awt.Color.RED), BLUE(2, java.awt.Color.BLUE), GREEN(3, java.awt.Color.GREEN), YELLOW(4,
+			java.awt.Color.YELLOW);
+
 	private int x;
-	
-	private ColorPlayer(int x){
-		this.x=x;
+
+	private java.awt.Color color;
+
+	private ColorPlayer(int x, java.awt.Color color) {
+		this.x = x;
+		this.color = color;
 	}
-	
-	public static ColorPlayer getColor(int x){
-		for(ColorPlayer el: ColorPlayer.values()){
-			if(el.x==x){
+
+	public static ColorPlayer getColor(int x) {
+		for (ColorPlayer el : ColorPlayer.values()) {
+			if (el.x == x) {
 				return el;
 			}
 		}
 		return null;
+	}
+	
+	public java.awt.Color getColor(){
+		return this.color;
 	}
 
 }
