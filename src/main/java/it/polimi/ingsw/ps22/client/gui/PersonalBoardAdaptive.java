6ddaba69d1c4864feedab76f26065f4557798f01
marcomussi <marco.mussi@mail.polimi.it>
2017-06-14 13:12:05 +0200

    posizionamento parametrico c

--- src/main/java/it/polimi/ingsw/ps22/client/gui/PersonalBoardAdaptive.java
+++ src/main/java/it/polimi/ingsw/ps22/client/gui/PersonalBoardAdaptive.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.ps22.client.gui;
 
 //	TUTTE LE POSIZIONI RITORNATE DA QUESTA CLASSE SONO DA RIFERIRSI AL FATTORE DI RISCALATURA
 //	DELLA PERSONAL BOARD E SONO IN POSIZIONE RELATIVA DALL'INIZIO DELLA PERSONAL BOARD 
-//  COMPRESA DI SPAZIO BONUS PERSONALI LE CUI MISURE REALI SONO UNIFORMATE A 220*1815
+//  COMPRESA DI SPAZIO BONUS PERSONALI LE CUI MISURE REALI SONO UNIFORMATE A 220*1836
 
 
 
@@ -13,10 +13,16 @@ public class PersonalBoardAdaptive {
 	private static final Rectangle woodSlot = new Rectangle(930,1050,2070,2190);
 	private static final Rectangle stoneSlot = new Rectangle(1500,1620,2070,2190);;
 	private static final Rectangle servantSlot = new Rectangle(2020,2140,2070,2190);
+	private static final Rectangle blackDiceButtonSlot = new Rectangle(0,400,4000,4600);
+	private static final Rectangle basicPlayerButtonSlot = new Rectangle(2100,2600,4000,4280);
 	private static final int cardOffsetX = 548;
 	private static final int cardTerritoryOffsetY = 1000;
-	private static final int cardVentureOffsetY = 2400;
-	private static final int cardCharacterOffsetY = 3200;
+	private static final int cardVentureOffsetY = 2350;
+	private static final int cardCharacterOffsetY = 3130;
+	private static final int diceButtonOffsetX = 500;
+	private static final int playerOffsetX = 600;
+	private static final int playerOffsetY = 315;
+	
 	
 	
 	public static Rectangle getBuildingCardSlot(double resizeFactor, int slot) {
@@ -75,6 +81,53 @@ public class PersonalBoardAdaptive {
 	
 	public static Rectangle getServantSlot(double resizeFactor) {
 		return servantSlot.resize(resizeFactor);
+	}
+	
+	public static Rectangle getBlackButtonSLot(double resizeFactor) {
+		return blackDiceButtonSlot.resize(resizeFactor);
+	}
+	
+	public static Rectangle getOrangeButtonSLot(double resizeFactor) {
+		Rectangle temp = new Rectangle(
+				(blackDiceButtonSlot.getInitx()+diceButtonOffsetX),
+				(blackDiceButtonSlot.getFinalx()+diceButtonOffsetX),
+				blackDiceButtonSlot.getInity(),
+				blackDiceButtonSlot.getFinaly());
+		return temp.resize(resizeFactor);
+	}
+	
+	public static Rectangle getWhiteButtonSLot(double resizeFactor) {
+		Rectangle temp = new Rectangle(
+				(blackDiceButtonSlot.getInitx()+(2*diceButtonOffsetX)),
+				(blackDiceButtonSlot.getFinalx()+(2*diceButtonOffsetX)),
+				blackDiceButtonSlot.getInity(),
+				blackDiceButtonSlot.getFinaly());
+		return temp.resize(resizeFactor);
+	}
+	
+	public static Rectangle getNeutralButtonSLot(double resizeFactor) {
+		Rectangle temp = new Rectangle(
+				(blackDiceButtonSlot.getInitx()+(3*diceButtonOffsetX)),
+				(blackDiceButtonSlot.getFinalx()+(3*diceButtonOffsetX)),
+				blackDiceButtonSlot.getInity(),
+				blackDiceButtonSlot.getFinaly());
+		return temp.resize(resizeFactor);
+	}
+	
+	//	DISTRIBUZIONE GIOCATORI: ESTENDIBILITA?
+	/*			GIOCATORE 1			GIOCATORE 3			GIOCATORE 5
+	 * 			GIOCATORE 2			GIOCATORE 4			ECC...
+	 */
+	
+	public static Rectangle getPlayerButtonSLot(double resizeFactor, int player) {
+		int xMolt = (int)(player/2);
+		int yMolt = 1-(player%2);
+		Rectangle temp = new Rectangle(
+				(basicPlayerButtonSlot.getInitx()+((xMolt)*playerOffsetX)),
+				(basicPlayerButtonSlot.getFinalx()+((xMolt)*playerOffsetX)),
+				(basicPlayerButtonSlot.getInity()+((yMolt)*playerOffsetY)),
+				(basicPlayerButtonSlot.getFinaly()+((yMolt)*playerOffsetY)));
+		return temp.resize(resizeFactor);
 	}
 	
 }
