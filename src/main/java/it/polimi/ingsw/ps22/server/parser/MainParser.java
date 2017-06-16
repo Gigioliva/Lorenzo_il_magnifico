cad97d57efb340ea9778c77398541138c073e956
marcomussi <marco.mussi@mail.polimi.it>
2017-06-16 10:37:08 +0200

    bugfix

--- src/main/java/it/polimi/ingsw/ps22/server/parser/MainParser.java
+++ src/main/java/it/polimi/ingsw/ps22/server/parser/MainParser.java
@@ -17,7 +17,7 @@ import it.polimi.ingsw.ps22.server.resource.VictoryPoint;
 
 public class MainParser {
 
-	public static void main(String[] args) {
+	public static void mainParser(String[] args) {
 		
 		ArrayList<CardBuilding> building = new ArrayList<CardBuilding>();
 		String pathnameBuilding = "src/main/java/it/polimi/ingsw/ps22/server/parser/resources/Building.xml";
