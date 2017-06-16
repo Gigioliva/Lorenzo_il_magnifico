cf7ec3b75cb87be21130d045ea5368b11817abfc
marcomussi <marco.mussi@mail.polimi.it>
2017-06-16 12:25:13 +0200

    bugfix

--- src/main/java/it/polimi/ingsw/ps22/client/gui/FamilyPath.java
+++ src/main/java/it/polimi/ingsw/ps22/client/gui/FamilyPath.java
@@ -1,14 +1,15 @@
 package it.polimi.ingsw.ps22.client.gui;
 
 
+import it.polimi.ingsw.ps22.server.model.Color;
 import it.polimi.ingsw.ps22.server.player.Family;
 import it.polimi.ingsw.ps22.server.player.Player;
 
 public class FamilyPath {
 
-	public static String getFamilyPathname(Player player, Family fam) {
+	public static String getFamilyPathname(java.awt.Color playerColor, Color fam) {
 		StringBuilder name = new StringBuilder(
-				"./image/family/" + player.getColor().toString()+"_"+fam.getColor().toString()
+				"./image/family/" + playerColor.toString() + "_" + fam.toString()
 				);
 		name = new StringBuilder(CardPath.addPNG(name.toString()));
 		return name.toString().toLowerCase();
