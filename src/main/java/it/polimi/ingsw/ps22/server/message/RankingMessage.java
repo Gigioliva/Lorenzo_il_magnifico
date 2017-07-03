8c5433f6f1490bc69933bc04e60813e57fb8fc34
marcomussi <marco.mussi@mail.polimi.it>
2017-07-03 11:56:48 +0200

    update top players

--- src/main/java/it/polimi/ingsw/ps22/server/message/RankingMessage.java
+++ src/main/java/it/polimi/ingsw/ps22/server/message/RankingMessage.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.ps22.server.message;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 
 import it.polimi.ingsw.ps22.client.main.VisitorB;
@@ -11,15 +12,31 @@ public class RankingMessage extends GenericMessage {
 
 	public RankingMessage(HashMap<String, UserData> login) {
 		StringBuilder str = new StringBuilder();
+		int vict;
+		int play;
+		DecimalFormat dec = new DecimalFormat("#.##");
+		;
+		HashMap<String, Float> temp = new HashMap<String, Float>();
+		str.append("<html>Top Players:<br/><br/>");
+		// Crea l'hashmap con i soli dati necessari
 		for (String el : login.keySet()) {
-			str.append("Player: " + el + " Giocate: " + login.get(el).getNumPlayedGame() + " Vinte: "
-					+ login.get(el).getNumVictory());
-			str.append("\n");
+			if (login.get(el).getNumPlayedGame() != 0) {
+				play = login.get(el).getNumPlayedGame();
+				vict = login.get(el).getNumVictory();
+				temp.put(el, ((float) vict / (float) play));
+			}
 		}
+
+		// Aggiunge alla stringa che sarà inviata nel messaggio
+		for (String el : temp.keySet()) {
+			str.append("Player: " + el + " Victory ratio: " + dec.format(temp.get(el)));
+			str.append("<br/>");
+		}
+		str.append("</html>");
 		setString(str.toString());
 	}
-	
-	public void accept(VisitorB visitor){
+
+	public void accept(VisitorB visitor) {
 		visitor.visit(this);
 	}
 }
